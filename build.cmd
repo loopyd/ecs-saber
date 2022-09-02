eb431a93d587dae57be1d81bb896b780559625b4
saber7ooth <nightwintertooth@gmail.com>
2022-09-02 13:38:04 -0700

    update build script

--- build.cmd
+++ build.cmd
@@ -7,4 +7,12 @@ if exist ./LeoEcsSaber (
     del /s /q LeoEcsSaber
 )
 git clone https://github.com/loopyd/ecs-saber.git
-xcopy "./ecs-saber/LeoEcsSaber" "./LeoEcsSaber" /e
+cd ./ecs-saber
+git checkout master
+git clean -dxf
+xcopy "./LeoEcsSaber" "../LeoEcsSaber" /a /e /k
+cd ../
+del /s /q ecs-saber
+dotnet restore
+dotnet build --configuration "Debug"
+del /s /q LeoEcsSaber
