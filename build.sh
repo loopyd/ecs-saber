cb913a82d9fd950747cb88c4fc56811c05c16f02
saber7ooth <nightwintertooth@gmail.com>
2022-09-02 13:48:47 -0700

    add script for building docs on linux

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+[ -d ./ecs-saber ] && rm -rfv ./ecs-saber
+[ -d ./LeoEcsSaber ] && rm -rfv ./LeoEcsSaber
+git clone https://github.com/loopyd/ecs-saber.git
+cd ./ecs-saber
+git checkout master
+git clean -dxf
+mkdir LeoEcsSaber
+cp -rfv ./LeoEcsSaber/* ../LeoEcsSaber/
+cd ../
+rm -rfv ./ecs-saber
+dotnet restore
+dotnet build --configuration "Debug"
+rm -rfv ./LeoEcsSaber
