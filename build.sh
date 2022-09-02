598a1984230fe377d34b022675be3b182e204534
saber7ooth <nightwintertooth@gmail.com>
2022-09-02 15:23:08 -0700

    Add more params to buildscript to make it generic

--- build.sh
+++ build.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
 CONFIGURATION="$1"
+SRC_BRANCH="$2"
+REPO_ORG="$3"
+REPO_PROJECT="$4"
+LIBRARY_NAME="$5"
 
-[ -d ./ecs-saber ] && rm -rfv ./ecs-saber
-[ -d ./LeoEcsSaber ] && rm -rfv ./LeoEcsSaber
-git clone https://github.com/loopyd/ecs-saber.git
-cd ./ecs-saber
-git checkout master
+[ -d "./${REPO_PROJECT}" ] && rm -rfv ./${REPO_PROJECT}
+[ -d "./${LIBRARY_NAME}" ] && rm -rfv ./${LIBRARY_NAME}
+git clone https://github.com/${REPO_ORG}/${REPO_PROJECT}.git
+cd ./${REPO_PROJECT}
+git checkout ${SRC_BRANCH}
 git clean -dxf
-mkdir ../LeoEcsSaber
-cp -rfv ./LeoEcsSaber/* ../LeoEcsSaber/
+mkdir ../${LIBRARY_NAME}
+cp -rfv ./${LIBRARY_NAME}/* ../${LIBRARY_NAME}/
 cd ../
-rm -rfv ./ecs-saber
+rm -rfv ./${REPO_PROJECT}
 dotnet restore
 dotnet build --configuration "${CONFIGURATION}"
-rm -rfv ./LeoEcsSaber
+rm -rfv ./${LIBRARY_NAME}
