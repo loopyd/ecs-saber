e93c4875821395f3cc432704942b42d16a6ae76f
saber7ooth <nightwintertooth@gmail.com>
2022-09-02 13:43:41 -0700

    fix xcopy command

--- build.cmd
+++ build.cmd
@@ -1,18 +1,18 @@
 @echo off
 
 if exist ./ecs-saber (
-    del /s /q ecs-saber
+    rmdir /s /q ecs-saber
 )
 if exist ./LeoEcsSaber (
-    del /s /q LeoEcsSaber
+    rmdir /s /q LeoEcsSaber
 )
 git clone https://github.com/loopyd/ecs-saber.git
 cd ./ecs-saber
 git checkout master
 git clean -dxf
-xcopy "./LeoEcsSaber" "../LeoEcsSaber" /a /e /k
+xcopy "./LeoEcsSaber" "../LeoEcsSaber" /a /e /k /i /y
 cd ../
-del /s /q ecs-saber
+rmdir /s /q ecs-saber
 dotnet restore
 dotnet build --configuration "Debug"
-del /s /q LeoEcsSaber
+rmdir /s /q LeoEcsSaber
